99d4d96c24e04abb0d2e4133c1755918e745b164
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-08-31 11:22:52 +0300

    Create about.php

---
+++ views/about.php
@@ -0,0 +1,22 @@
+<?php
+include "views/partials/head.php";
+include "views/partials/navbar.php";
+?>
+<div class="container-fluid bg-dark text-light">
+ <div class="container">
+  <div class="row">
+    <div class="col-md-3 m-t">
+     <img src="https://avatars.githubusercontent.com/u/105741983?v=4" class="w-100 rounded">
+    </div>
+    <div class="col-md-9 m-t">
+     "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+     "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."<br><br>
+     <a href="https://github.com/hasan-kilici"><i class="fab fa-github social-icon"></i></a>
+    </div>
+  </div>
+ </div>
+</div>
+<div class="container"><br>
+  <h4>My Projects</h4>
+  <hr>
+</div>
