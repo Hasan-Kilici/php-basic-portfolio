35a01a8055e725e98bfdae33d9144f2fc419e764
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-08-31 11:22:09 +0300

    Create 404.php

---
+++ views/404.php
@@ -0,0 +1,7 @@
+<?php
+include "views/partials/head.php";
+?>
+<div class="error-box">
+  <i class="far fa-frown err-icon"></i>
+  <h4>Böyle bir sayfa bulunamadı <a href="/">Anasayfaya dön</a></h4>
+</div>
